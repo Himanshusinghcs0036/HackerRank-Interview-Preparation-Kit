fef9d074ec5deead760f1e6db546bbb6413a946b
Himanshu Singh <Hsingh@XEBIAINDIA.LOCAL>
2018-10-16 14:39:43 +0530

    Counting Value Solution

--- build/warm_up/CountingValleys.scala
+++ build/warm_up/CountingValleys.scala
@@ -6,16 +6,15 @@ object CountingValleys extends App {
     var currentStatus = 0
     var countVally = 0
     for (step <- s.split("")) {
-      if (currentStatus == 0)
-        if (step == "D")  countVally = countVally + 1
+      if (currentStatus == 0 & step == "D") countVally = countVally + 1
       currentStatus=updateLevel(step, currentStatus)
     }
     return countVally
   }
 
   def updateLevel(step: String, currentStatus: Int): Int = {
-    if (step == "D")  return (currentStatus + 1)
-    else  return (currentStatus - 1)
+    if (step == "D")  return (currentStatus - 1)
+    else  return (currentStatus + 1)
   }
 
   println("total Vally =  "+countingValleys(5, "UDDDUDUU"))
